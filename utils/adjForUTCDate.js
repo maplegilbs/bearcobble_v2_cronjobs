0b497adbd76137895edabc0cc15d64b0ef502f07
Scott Gilbert <scottvtgilbert@gmail.com>
2023-03-02 17:39:46 -0500

    Updating UTC adjuster function to static offset of -5 hours as using dynamic is affected by heroku's clocks using utc

--- utils/adjForUTCDate.js
+++ utils/adjForUTCDate.js
@@ -1,7 +1,7 @@
 export function adjustForUTC(timeToConvert){
     console.log(timeToConvert)
     let currentHours = timeToConvert.getUTCHours();
-    let hourDiff = timeToConvert.getTimezoneOffset()/60;
+    let hourDiff = 5;
     timeToConvert.setUTCHours(currentHours-hourDiff);
     console.log(timeToConvert)
     return timeToConvert;
