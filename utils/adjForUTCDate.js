5eb19f09094ef3360873ba7e20f194163f991ea7
Scott Gilbert <scottvtgilbert@gmail.com>
2023-03-02 17:35:43 -0500

    Adding console logs to UTC datetime adjustment for debugging

--- utils/adjForUTCDate.js
+++ utils/adjForUTCDate.js
@@ -1,6 +1,8 @@
 export function adjustForUTC(timeToConvert){
+    console.log(timeToConvert)
     let currentHours = timeToConvert.getUTCHours();
     let hourDiff = timeToConvert.getTimezoneOffset()/60;
     timeToConvert.setUTCHours(currentHours-hourDiff);
+    console.log(timeToConvert)
     return timeToConvert;
 }
