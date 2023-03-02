b91a029c61500657d9c38451803ad5c283d556c1
Scott Gilbert <scottvtgilbert@gmail.com>
2023-03-01 20:47:33 -0500

    Initial commit for testing remote access to sql database on inmotion

---
+++ dataFetchTest.js
@@ -0,0 +1,28 @@
+//Libraries
+import mysql from 'mysql2';
+import dotenv from 'dotenv'
+dotenv.config();
+
+const pool = mysql.createPool(
+    {
+        host: process.env.REMOTE_HOST,
+        user: process.env.REMOTE_USER,
+        password: process.env.REMOTE_PASSWORD,
+        database: process.env.REMOTE_DATABASE
+    }
+).promise()
+
+const getSomeData = async (pastTime) => {
+    const queryResult = await pool.query(
+        `
+        select *
+        from vacuumlogs
+        order by recordDateTime desc
+        limit 10
+        `
+    )
+    return queryResult[0]
+}
+
+const queryResults = await getSomeData(24);
+console.log(queryResults)
